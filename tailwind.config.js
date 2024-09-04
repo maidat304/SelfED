4625d697d1a37fc166f5c21027058e0da317ead6
Mai Thành Đạt <110539892+maidat304@users.noreply.github.com>
2024-09-04 15:58:31 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-import welcome_bg from "./src/assets/welcome-bg.webp";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       backgroundImage: {
-        balloon: "url(${welcome_bg})",
+        balloon: "url('/src/assets/welcome-bg.webp')",
         "custom-gradient":
           "linear-gradient(193deg, #6C8BC5 2.78%, #581C83 99.77%)",
         "ornament-color":

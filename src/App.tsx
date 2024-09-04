55cd02ca903016a2461cde8ff663fb4dd35c49b3
maidat304 <maithanhdat102@gmail.com>
2024-09-04 14:23:16 +0700

    ADD: login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Welcome from './pages/Welcome'
+import Login from './pages/Login'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Welcome />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ export default {
     extend: {
       backgroundImage: {
         balloon: "url('./src/assets/welcome-bg.webp')",
+        "custom-gradient":
+          "linear-gradient(193deg, #6C8BC5 2.78%, #581C83 99.77%)",
+        "ornament-color":
+          "linear-gradient(225deg, #D69BFD 14.43%, #9672FF 83.78%)",
       },
     },
   },
